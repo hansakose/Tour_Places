d8481caa3f993ef44993f8542d01702ad12ad23a
HansaKose <hansakose1@gmail.com>
2022-07-22 22:51:22 +0200

    done

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.scss";
+
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+
+const App = () => {
+  return (
+    <div className="App">
+     
+
+      <Navbar/>
+      <Header/>
+      <Main/>
+     
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Main/Card.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { data } from '../../helpers/data'
+
+const Card = (data) => {
+  
+  const {id,title,image,desc} = data;
+
+  return (
+    <div className="cards">
+        <div className="title">
+            <h1>{title}</h1>
+        </div>
+        <img src={image} alt="" />
+
+        <div className="card-over">
+            <p>
+              {desc}
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default Card
